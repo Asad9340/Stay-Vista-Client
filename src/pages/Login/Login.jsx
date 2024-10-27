68dcc58afb402a4ab5776a1e8cefb13595f819d2
Asad9340 <asadulimran1999@gmail.com>
2024-10-27 13:30:39 +0600

    forget password functionality added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,9 +7,11 @@ import { useState } from 'react';
 import { IoIosEyeOff, IoMdEye } from 'react-icons/io';
 
 const Login = () => {
+  const [forgetPass, setForgatePass] = useState(false);
   const navigate = useNavigate();
   const [eyeIcon, setEyeIcon] = useState(false);
-  const { signInWithGoogle, loading, setLoading, signIn } = useAuth();
+  const { signInWithGoogle, loading, setLoading, signIn, resetPassword } =
+    useAuth();
   const onGoogleClickHandle = async () => {
     try {
       await signInWithGoogle();
@@ -29,9 +31,7 @@ const Login = () => {
     const email = form.email.value;
     const password = form.password.value;
     try {
-      const result = await signIn(email, password);
-      console.log(result);
-
+      await signIn(email, password);
       navigate('/');
       toast.success('LogIn Successfully');
     } catch (e) {
@@ -39,108 +39,187 @@ const Login = () => {
       setLoading(false);
     }
   };
+  const handleForgatePassword = () => {
+    setForgatePass(!forgetPass);
+  };
+  const handleResetPassword = async e => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    console.log(email);
+    try {
+      await resetPassword(email);
+      toast.success('Password reset email sent.');
+    } catch (e) {
+      toast.error(e.message);
+    }
+  };
+  const goToLogin = () => {
+    setForgatePass(!forgetPass);
+  };
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
-        <div className="mb-8 text-center">
-          <h1 className="my-3 text-4xl font-bold">Log In</h1>
-          <p className="text-sm text-gray-400">
-            Sign in to access your account
-          </p>
-        </div>
-        <form
-          onSubmit={handleLogIn}
-          noValidate=""
-          action=""
-          className="space-y-6 ng-untouched ng-pristine ng-valid"
-        >
-          <div className="space-y-4">
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                required
-                placeholder="Enter Your Email Here"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
-                data-temp-mail-org="0"
-              />
-            </div>
-            <div className="relative">
-              <div className="flex justify-between">
-                <label htmlFor="password" className="text-sm mb-2">
-                  Password
-                </label>
-              </div>
-              <input
-                type={!eyeIcon ? 'password' : 'text'}
-                name="password"
-                autoComplete="current-password"
-                id="password"
-                required
-                placeholder="*******"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
-              />
-              <p className="absolute right-3 bottom-3">
-                {eyeIcon ? (
-                  <IoMdEye onClick={handelEyeClick} title='Hide Password'/>
-                ) : (
-                  <IoIosEyeOff onClick={handelEyeClick} title='Show Password' />
-                )}
+    <>
+      {!forgetPass ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+            <div className="mb-8 text-center">
+              <h1 className="my-3 text-4xl font-bold">Log In</h1>
+              <p className="text-sm text-gray-400">
+                Sign in to access your account
               </p>
             </div>
-          </div>
-
-          <div>
-            <button
-              disabled={loading}
-              type="submit"
-              className="bg-rose-500 w-full rounded-md py-3 text-white"
+            <form
+              onSubmit={handleLogIn}
+              noValidate=""
+              action=""
+              className="space-y-6 ng-untouched ng-pristine ng-valid"
             >
-              {loading ? (
-                <ImSpinner3 className="animate-spin m-auto" />
-              ) : (
-                'Continue'
-              )}
-            </button>
-          </div>
-        </form>
-        <div className="space-y-1">
-          <button className="text-xs hover:underline hover:text-rose-500 text-gray-400">
-            Forgot password?
-          </button>
-        </div>
-        <div className="flex items-center pt-4 space-x-1">
-          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-          <p className="px-3 text-sm dark:text-gray-400">
-            Login with social accounts
-          </p>
-          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-        </div>
-        <button
-          onClick={onGoogleClickHandle}
-          disabled={loading}
-          className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
-        >
-          <FcGoogle size={32} />
+              <div className="space-y-4">
+                <div>
+                  <label htmlFor="email" className="block mb-2 text-sm">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    required
+                    placeholder="Enter Your Email Here"
+                    className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                    data-temp-mail-org="0"
+                  />
+                </div>
+                <div className="relative">
+                  <div className="flex justify-between">
+                    <label htmlFor="password" className="text-sm mb-2">
+                      Password
+                    </label>
+                  </div>
+                  <input
+                    type={!eyeIcon ? 'password' : 'text'}
+                    name="password"
+                    autoComplete="current-password"
+                    id="password"
+                    required
+                    placeholder="*******"
+                    className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                  />
+                  <p className="absolute right-3 bottom-3">
+                    {eyeIcon ? (
+                      <IoMdEye onClick={handelEyeClick} title="Hide Password" />
+                    ) : (
+                      <IoIosEyeOff
+                        onClick={handelEyeClick}
+                        title="Show Password"
+                      />
+                    )}
+                  </p>
+                </div>
+              </div>
 
-          <p>Continue with Google</p>
-        </button>
-        <p className="px-6 text-sm text-center text-gray-400">
-          Don&apos;t have an account yet?{' '}
-          <Link
-            to="/signup"
-            className="hover:underline hover:text-rose-500 text-gray-600"
-          >
-            Sign up
-          </Link>
-          .
-        </p>
-      </div>
-    </div>
+              <div>
+                <button
+                  disabled={loading}
+                  type="submit"
+                  className="bg-rose-500 w-full rounded-md py-3 text-white"
+                >
+                  {loading ? (
+                    <ImSpinner3 className="animate-spin m-auto" />
+                  ) : (
+                    'Continue'
+                  )}
+                </button>
+              </div>
+            </form>
+            <div className="space-y-1">
+              <button
+                onClick={handleForgatePassword}
+                className="text-xs hover:underline hover:text-rose-500 text-gray-400"
+              >
+                Forgot password?
+              </button>
+            </div>
+            <div className="flex items-center pt-4 space-x-1">
+              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+              <p className="px-3 text-sm dark:text-gray-400">
+                Login with social accounts
+              </p>
+              <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+            </div>
+            <button
+              onClick={onGoogleClickHandle}
+              disabled={loading}
+              className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
+            >
+              <FcGoogle size={32} />
+
+              <p>Continue with Google</p>
+            </button>
+            <p className="px-6 text-sm text-center text-gray-400">
+              Don&apos;t have an account yet?{' '}
+              <Link
+                to="/signup"
+                className="hover:underline hover:text-rose-500 text-gray-600"
+              >
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="flex flex-col max-w-lg p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+            <div className="mb-8 text-center">
+              <h1 className="my-3 text-4xl font-bold">Reset Password</h1>
+              <p className="text-sm text-gray-400">
+                Please Enter Email for reset password
+              </p>
+            </div>
+            <form
+              onSubmit={handleResetPassword}
+              noValidate=""
+              action=""
+              className="space-y-6 ng-untouched ng-pristine ng-valid"
+            >
+              <div className="space-y-4">
+                <div>
+                  <label htmlFor="email" className="block mb-2 text-sm">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    required
+                    placeholder="Enter Your Email Here"
+                    className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                    data-temp-mail-org="0"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  disabled={loading}
+                  type="submit"
+                  className="bg-rose-500 w-full rounded-md py-3 text-white"
+                >
+                  Reset Password
+                </button>
+              </div>
+            </form>
+            <p className="px-6 text-sm text-center text-gray-400">
+              Reset Successfully?
+              <p
+                onClick={goToLogin}
+                className="hover:underline hover:text-rose-500 text-gray-600 inline"
+              >
+                LogIn
+              </p>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
