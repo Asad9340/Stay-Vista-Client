13c579d2ca90e329496378a33fa41da91242c2c7
Asad9340 <asadulimran1999@gmail.com>
2024-11-14 01:01:33 +0600

    added new route for manage booking and my booking

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -16,6 +16,8 @@ import Profile from '../pages/Dashboard/Common/Profile';
 import ManageUsers from '../pages/Dashboard/Admin/ManageUsers';
 import AdminRoute from './AdminRoute';
 import HostRoute from './HostRoute';
+import MyBookings from '../pages/Dashboard/Guest/MyBookings';
+import ManageBookings from '../pages/Dashboard/Host/ManageBookings';
 
 export const router = createBrowserRouter([
   {
@@ -92,12 +94,30 @@ export const router = createBrowserRouter([
         ),
       },
       {
+        path: 'my-bookings',
+        element: (
+          <PrivateRoute>
+            <MyBookings />
+          </PrivateRoute>
+        ),
+      },
+      {
         path: 'manage-users',
         element: (
           <PrivateRoute>
             <AdminRoute>
               <ManageUsers />
             </AdminRoute>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'manage-bookings',
+        element: (
+          <PrivateRoute>
+            <HostRoute>
+              <ManageBookings />
+            </HostRoute>
           </PrivateRoute>
         ),
       },
