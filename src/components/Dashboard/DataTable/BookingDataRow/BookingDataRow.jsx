2835892aaf2095fbc920b4d65564c11ff26b6234
Asad9340 <asadulimran1999@gmail.com>
2024-11-16 21:09:51 +0600

    added cancel data

--- src/components/Dashboard/DataTable/BookingDataRow/BookingDataRow.jsx
+++ src/components/Dashboard/DataTable/BookingDataRow/BookingDataRow.jsx
@@ -1,7 +1,14 @@
 import { format } from 'date-fns';
 import PropTypes from 'prop-types';
+import DeleteModal from '../../../Modal/DeleteModal';
 
-const BookingDataRow = ({ booking }) => {
+const BookingDataRow = ({
+  booking,
+  closeModal,
+  isOpen,
+  setIsOpen,
+  handleDelete,
+}) => {
   return (
     <tr>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -52,13 +59,22 @@ const BookingDataRow = ({ booking }) => {
         </p>
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <span className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+        <button
+          onClick={() => setIsOpen(true)}
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
           <span
             aria-hidden="true"
             className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
           ></span>
           <span className="relative">Cancel</span>
-        </span>
+        </button>
+        <DeleteModal
+          closeModal={closeModal}
+          isOpen={isOpen}
+          id={booking?.roomId}
+          handleDelete={handleDelete}
+        />
       </td>
     </tr>
   );
--- src/pages/Dashboard/Guest/MyBookings.jsx
+++ src/pages/Dashboard/Guest/MyBookings.jsx
@@ -4,22 +4,47 @@ import { useQuery } from '@tanstack/react-query';
 import LoadingSpinner from '../../../components/Shared/LoadingSpinner';
 import useAuth from '../../../hooks/useAuth';
 import BookingDataRow from '../../../components/Dashboard/DataTable/BookingDataRow/BookingDataRow';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 const MyBookings = () => {
   const axiosSecure = useAxiosSecure();
-  const {user}=useAuth()
-    const {
-      data: myBooking = [],
-      isLoading,
-      refetch
-    } = useQuery({
-      queryKey: ['myBooking'],
-      queryFn: async () => {
-        const { data } = await axiosSecure(`/booking/${user?.email}`);
-        return data;
-      },
-    });
-    if (isLoading) return <LoadingSpinner />;
+  const { user } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+  const {
+    data: myBooking = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ['myBooking'],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/booking/${user?.email}`);
+      return data;
+    },
+  });
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const handleDelete = async id => {
+    try {
+      const result = await axiosSecure.patch(`/room/status/${id}`, {
+        status: false,
+      });
+      if (result.data.modifiedCount > 0) {
+        const deletedResult = await axiosSecure.delete(`/booking/cancel/${id}`);
+        if (deletedResult.status === 200) {
+          toast.success('Cancelled Booking Successfully');
+          refetch();
+        } else {
+          toast.error('Failed to cancel Booking');
+        }
+      }
+    } catch (e) {
+      toast.error(e.message);
+    }
+    closeModal();
+  };
+  if (isLoading) return <LoadingSpinner />;
   return (
     <>
       <Helmet>
@@ -28,58 +53,72 @@ const MyBookings = () => {
 
       <div className="container mx-auto px-4 sm:px-8">
         <div className="py-8">
-          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-              <table className="min-w-full leading-normal">
-                <thead>
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      Title
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      Info
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      Price
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      From
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      To
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                    >
-                      Action
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* Table Row Data */}
-                  {myBooking.map(user => (
-                    <BookingDataRow key={user._id} user={user} refetch={refetch} />
-                  ))}
-                </tbody>
-              </table>
+          {myBooking.length > 0 ? (
+            <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+              <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table className="min-w-full leading-normal">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Title
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Info
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Price
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        From
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        To
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Action
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* Table Row Data */}
+                    {myBooking.map(booking => (
+                      <BookingDataRow
+                        key={booking._id}
+                        booking={booking}
+                        refetch={refetch}
+                        closeModal={closeModal}
+                        isOpen={isOpen}
+                        setIsOpen={setIsOpen}
+                        handleDelete={handleDelete}
+                      />
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="text-center py-12">
+              <p className="text-gray-600">No bookings found</p>
+            </div>
+          )}
         </div>
       </div>
     </>
